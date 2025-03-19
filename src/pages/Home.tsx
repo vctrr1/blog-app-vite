047e685c9cacd7ba2694a00882175a80d436727c
vctrr1 <victoremaanoel@outlook.com>
2025-03-19 01:46:02 -0300

    feat: create navBart component and install some shadcn components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import ToggleButton from "@/components/toggle-theme";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <ToggleButton />
+    </div>
+  );
+};
+
+export default Home;
