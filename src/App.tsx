fe28ff9f4959901129895867bae8fd8f4afa9e6e
vctrr1 <victoremaanoel@outlook.com>
2025-03-21 03:33:25 -0300

    chore: install some components and rename a file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import { ThemeProvider } from "./components/theme-provider";
 import NavBar from "./components/nav-bar";
+import CreatePostPage from "./pages/create-post-page";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="container mx-auto px-5 py-5">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreatePostPage />} />
           </Routes>
         </div>
       </div>
