c3657b455e00c2ece9ba8f8c90a35a6a201d335d
vctrr1 <victoremaanoel@outlook.com>
2025-03-21 00:27:55 -0300

    feat: make navbar show user image and make login and logout

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,28 +1,92 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import ToggleTheme from "./toggle-theme";
+import { useAuth } from "@/hooks/use-auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+  const { signInWithGitHub, signOut, user } = useAuth();
+  const displayName = user?.user_metadata.user_name;
 
   return (
     <nav className="fixed top-0 w-full z-40  border-b">
       <div className="max-w-5xl mx-auto px-4">
         <div className=" flex justify-between h-16 items-center">
-          <div>
-            <Link to="/" className="font-mono text-xl font-bold">
-              blog<span className="text-purple-500">.app</span>
-            </Link>
-          </div>
+          <Link to="/" className="font-mono text-xl font-bold">
+            blog<span className="text-purple-500">.app</span>
+          </Link>
+
           {/** Desktop links */}
           <div className="items-center space-x-8 hidden md:flex">
-            <Link to={"/"} className="">
+            <Link
+              to={"/"}
+              className={`${
+                location.pathname === "/" ? "text-muted-foreground" : ""
+              }`}
+            >
               Home
             </Link>
-            <Link to={"/create"}>Criar Post</Link>
-            <Link to={"/communities"}>Comunidades</Link>
-            <Link to={"/community/create"}>Criar Comunidades</Link>
+            <Link
+              to={"/create"}
+              className={`${
+                location.pathname === "/create" ? "text-muted-foreground" : ""
+              }`}
+            >
+              Criar Post
+            </Link>
+            <Link
+              to={"/communities"}
+              className={`${
+                location.pathname === "/communities"
+                  ? "text-muted-foreground"
+                  : ""
+              }`}
+            >
+              Comunidades
+            </Link>
+            <Link
+              to={"/community/create"}
+              className={`${
+                location.pathname === "/community/create"
+                  ? "text-muted-foreground"
+                  : ""
+              }`}
+            >
+              Criar Comunidades
+            </Link>
             <ToggleTheme />
+          </div>
+
+          {/* desltop auth */}
+          <div className="hidden md:flex items-center">
+            {user ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex items-center gap-1">
+                  <img
+                    src={user.user_metadata?.avatar_url}
+                    className="w-8 h-8 object-cover rounded-full"
+                  />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem disabled>{displayName}</DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <button onClick={signOut}>Sair</button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              <Button variant="outline" onClick={signInWithGitHub}>
+                Entrar
+              </Button>
+            )}
           </div>
 
           {/** Mobile menu button */}
@@ -60,31 +124,29 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+
       {/** Mobile links */}
       {menuOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 bg-background">
-            <Link
-              to={"/"}
-              className="block px-3 py-2 rounded-md text-base font-medium"
-            >
+            <Link to={"/"} className="block px-3 py-2 rounded-md text-base">
               Home
             </Link>
             <Link
               to={"/create"}
-              className="block px-3 py-2 rounded-md text-base font-medium"
+              className="block px-3 py-2 rounded-md text-base"
             >
               Criar Post
             </Link>
             <Link
               to={"/communities"}
-              className="block px-3 py-2 rounded-md text-base font-medium "
+              className="block px-3 py-2 rounded-md text-base "
             >
               Comunidades
             </Link>
             <Link
               to={"/community/create"}
-              className="block px-3 py-2 rounded-md text-base font-medium "
+              className="block px-3 py-2 rounded-md text-base"
             >
               Criar Comunidades
             </Link>
