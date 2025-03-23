912ae0088171f17e0aa4c8587f9e40abb928e987
vctrr1 <victoremaanoel@outlook.com>
2025-03-23 15:51:49 -0300

    feat: add react query to create a post in supabase

--- src/components/create-post-form.tsx
+++ src/components/create-post-form.tsx
@@ -4,7 +4,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { supabase } from "../../supabase-client";
 
@@ -13,8 +13,23 @@ interface PostInput {
   content: string;
 }
 
-const createPost = async (post: PostInput) => {
-  const { data, error } = await supabase.from("post").insert(post);
+const createPost = async (post: PostInput, imageFile: File) => {
+  const filePath = `${post.title}-${Date.now()}-${imageFile.name}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from("post-images")
+    .upload(filePath, imageFile);
+
+  if (uploadError) throw new Error(uploadError.message);
+
+  const { data: publicURLData } = supabase.storage
+    .from("post-images")
+    .getPublicUrl(filePath);
+
+  const { data, error } = await supabase
+    .from("post")
+    .insert({ ...post, image_url: publicURLData.publicUrl });
+
   if (error) {
     throw new Error(error.message);
   }
@@ -22,14 +37,26 @@ const createPost = async (post: PostInput) => {
 };
 
 const CreatePostForm = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  const { mutate } = useMutation({ mutationFn: createPost });
+  const { mutate } = useMutation({
+    mutationFn: (data: { post: PostInput; imageFile: File }) => {
+      return createPost(data.post, data.imageFile);
+    },
+  });
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    mutate({ title, content });
+    if (!selectedFile) return;
+    mutate({ post: { title, content }, imageFile: selectedFile });
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+    }
   };
 
   return (
@@ -49,7 +76,7 @@ const CreatePostForm = () => {
           <Label>Conteudo:</Label>
           <Textarea
             id="content"
-            placeholder="..."
+            className="h-full max-h-[300px] md:max-h-none overflow-auto scrollbar-hide"
             required
             rows={10}
             onChange={(event) => setContent(event.target.value)}
@@ -57,7 +84,12 @@ const CreatePostForm = () => {
         </div>
         <div className="grid w-full items-center gap-1.5">
           <Label htmlFor="picture">Foto:</Label>
-          <Input id="picture" type="file" />
+          <Input
+            id="picture"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
         </div>
       </div>
       <Button type="submit" className="w-full rounded-base">
