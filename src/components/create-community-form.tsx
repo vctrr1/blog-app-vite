66b0ca3537d4d1a69044919b566bb6c375e3201d
vctrr1 <victoremaanoel@outlook.com>
2025-03-27 20:32:40 -0300

    feat: create community page and insert in db

---
+++ src/components/create-community-form.tsx
@@ -0,0 +1,88 @@
+import { Loader2Icon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+
+import React, { useRef, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../../supabase-client";
+//import { useAuth } from "@/hooks/use-auth";
+import { useNavigate } from "react-router";
+
+interface CommunityInput {
+  name: string;
+  description: string;
+}
+
+const createCommunity = async (community: CommunityInput) => {
+  const { error, data } = await supabase.from("communities").insert({
+    name: community.name,
+    description: community.description,
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+};
+
+const CreateCommunityForm = () => {
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const formref = useRef<HTMLFormElement>(null);
+  //const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: createCommunity,
+    onSuccess: () => {
+      navigate("/communities");
+    },
+  });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    mutate({ name, description });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} ref={formref}>
+      <div className="flex flex-col gap-4 mb-7">
+        <div className="grid w-full items-center gap-1.5">
+          <Label>Nome:</Label>
+          <Input
+            type="text"
+            id="name"
+            placeholder=""
+            required
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div className="grid w-full items-center gap-1.5">
+          <Label>Descrição:</Label>
+          <Textarea
+            id="description"
+            className="h-full max-h-[300px] md:max-h-none overflow-auto scrollbar-hide"
+            required
+            rows={10}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </div>
+      </div>
+      <Button
+        type="submit"
+        className="w-full rounded-base"
+        disabled={isPending}
+      >
+        Criar
+        {isPending && <Loader2Icon className="animate-spin" />}
+      </Button>
+      {error && (
+        <div className="flex justify-center p-4">
+          <p>Erro ao criar comunidade, usuario precisa estar logado</p>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default CreateCommunityForm;
--- src/pages/create-communities.tsx
+++ src/pages/create-communities.tsx
@@ -1,7 +1,26 @@
+import CreateCommunityForm from "@/components/create-community-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const CreateCommunities = () => {
   return (
-    <div>
-      <div>Test</div>
+    <div className="w-full h-full flex justify-center">
+      <Card className="w-full md:w-[80%] max-w-[700px]">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl">Criar nova comunidade</CardTitle>
+          <CardDescription className="text-muted-foreground text-base">
+            Crie um lugar para compartilhar suas ideias.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CreateCommunityForm />
+        </CardContent>
+      </Card>
     </div>
   );
 };
