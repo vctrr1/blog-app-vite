6003792b36cd94d596ce5dcec8b1219c0bcfdf73
vctrr1 <victoremaanoel@outlook.com>
2025-03-28 03:10:57 -0300

    chore: resolving vite problems

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/vctrr1/social-media-app",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
